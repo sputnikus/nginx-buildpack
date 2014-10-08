faf8e66571d659abd019efb227d4a61b2b7fe623
David Genord II <david@collectiveidea.com>
2014-10-08 12:12:07 -0400

    A lot of nginx tweaks

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -1,60 +1,52 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Build NGINX and modules on Heroku.
-# This program is designed to run in a web dyno provided by Heroku.
-# We would like to build an NGINX binary for the builpack on the
-# exact machine in which the binary will run.
-# Our motivation for running in a web dyno is that we need a way to
-# download the binary once it is built so we can vendor it in the buildpack.
-#
-# Once the dyno has is 'up' you can open your browser and navigate
-# this dyno's directory structure to download the nginx binary.
 
-NGINX_VERSION=1.5.7
-PCRE_VERSION=8.21
-HEADERS_MORE_VERSION=0.23
+# fail hard
+set -o pipefail
+
+NGINX_VERSION=1.6.2
+PCRE_VERSION=8.34
+
+dep_dirname=nginx-${NGINX_VERSION}
+dep_archive_name=${dep_dirname}.tar.gz
+dep_url=http://nginx.org/download/${dep_archive_name}
 
 cache_folder=$2/nginx-build
-cache_file=${cache_folder}/nginx-${NGINX_VERSION}-${PCRE_VERSION}-${HEADERS_MORE_VERSION}
+mkdir -p $cache_folder
+cache_file=${cache_folder}/nginx-${NGINX_VERSION}-${PCRE_VERSION}
 
 if [ -f $cache_file ]; then
-	echo "Using cached build"
+	echo "-----> Using cached build of Nginx ${NGINX_VERSION}"
 else
+	echo "-----> Building Nginx ${NGINX_VERSION}"
 	nginx_tarball_url=http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
 	pcre_tarball_url=http://garr.dl.sourceforge.net/project/pcre/pcre/${PCRE_VERSION}/pcre-${PCRE_VERSION}.tar.bz2
-	headers_more_nginx_module_url=https://github.com/agentzh/headers-more-nginx-module/archive/v${HEADERS_MORE_VERSION}.tar.gz
 
 	temp_dir=$(mktemp -d /tmp/nginx.XXXXXXXXXX)
-
-	echo "Cache directory: $2"
-
 	cd $temp_dir
-	echo "Temp dir: $temp_dir"
 
-	echo "Downloading $nginx_tarball_url"
 	curl -L $nginx_tarball_url | tar xz
 
-	echo "Downloading $pcre_tarball_url"
-	(cd nginx-${NGINX_VERSION} && curl -L $pcre_tarball_url | tar xj )
+	pushd nginx-${NGINX_VERSION}
+	curl -L $pcre_tarball_url | tar xj
 
-	echo "Downloading $headers_more_nginx_module_url"
-	(cd nginx-${NGINX_VERSION} && curl -L $headers_more_nginx_module_url | tar xz )
+	echo "-----> Configuring nginx"
+	./configure \
+		--http-client-body-temp-path=/app/tmp/var/run/nginx/client_body_temp \
+		--http-proxy-temp-path=/app/tmp/var/run/nginx/proxy_temp \
+		--with-pcre=pcre-${PCRE_VERSION} \
+		--with-http_realip_module \
+		--with-http_gzip_static_module > /dev/null
+	echo "-----> Building nginx"
+	make -s -j 9
 
-	(
-		cd nginx-${NGINX_VERSION}
-		echo "Configuring nginx"
-		./configure \
-			--with-pcre=pcre-${PCRE_VERSION} \
-			--prefix=/tmp/nginx \
-			--add-module=/${temp_dir}/nginx-${NGINX_VERSION}/headers-more-nginx-module-${HEADERS_MORE_VERSION} \
-			--with-http_gzip_static_module > /dev/null
-		echo "Building nginx"
-		make > /dev/null
-
-		# Remove old cached versions
-		rm -rf $cache_folder
-		mkdir -p $cache_folder
-		cp objs/nginx $cache_file
-	)
+	# Remove old cached versions
+	rm -f $cache_folder/*
+	cp objs/nginx $cache_file
+	popd
 fi
 
 cp $cache_file $1
+mkdir -p /app/tmp/var/run/nginx
+
+echo "-----> Done."
